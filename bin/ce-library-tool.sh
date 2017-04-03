9892bf71934db59906e3824af441f8d03816b840
Boris Schauer <me@bschauer.de>
2017-04-03 14:14:22 +0200

    [TASK] Add Library Tool

--- bin/ce-library-tool.sh
+++ bin/ce-library-tool.sh
@@ -7,4 +7,24 @@ select_ctype_from_lib () {
 
 select_ctype_from_lib
 
-echo ${extname}
+libcTypeUpper=${libcType};
+libcTypeUpper=`echo ${libcTypeUpper:0:1} | tr  '[a-z]' '[A-Z]'`${libcTypeUpper:1}
+
+# Templates
+cp ${libdir}/cTypes/${libcType}/FE_${libcTypeUpper}.html ${extensiondir}/Resources/Private/Templates/ContentElements/FE_${libcTypeUpper}.html
+cp ${libdir}/cTypes/${libcType}/BE_${libcTypeUpper}.html ${extensiondir}/Resources/Private/BackendPreviews/BE_${libcTypeUpper}.html
+
+# Typoscript
+cp ${libdir}/cTypes/${libcType}/pagets_${libcType}.t3s ${extensiondir}/Configuration/PageTS/ContentElements/pagets_${libcType}.t3s
+sed -i '' "s/provider/${extname}/g" ${extensiondir}/Configuration/PageTS/ContentElements/pagets_${libcType}.t3s
+
+cp ${libdir}/cTypes/${libcType}/typoscript_${libcType}.t3s ${extensiondir}/Configuration/TypoScript/ContentElements/typoscript_${libcType}.t3s
+sed -i '' "s/provider/${extname}/g" ${extensiondir}/Configuration/TypoScript/ContentElements/typoscript_${libcType}.t3s
+
+# TCA
+cp ${libdir}/cTypes/${libcType}/tt_content_${libcType}.php ${extensiondir}/Configuration/TCA/Overrides/tt_content_${libcType}.php
+sed -i '' "s/provider/${extname}/g" ${extensiondir}/Configuration/TCA/Overrides/tt_content_${libcType}.php
+
+echo
+echo "Your new Content Element is ready!"
+echo

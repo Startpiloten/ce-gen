62e77873ef44de29e2690b7c27d56af229480975
Boris Schauer <me@bschauer.de>
2017-09-15 13:14:04 +0200

    [TASK] IRRE generator updated for startpilot2

--- bin/irre-generator.sh
+++ bin/irre-generator.sh
@@ -8,26 +8,27 @@ export cedescription
 
 ## Backend Preview
 mkdir -p $extensiondir/Resources/Private/BackendPreviews
-envsubst '${extname} ${cename} ${cenameUpper} ${cetitle} ${cedescription}' < $bindir/lib/irre/BE_ctype.html > $extensiondir/Resources/Private/BackendPreviews/BE_${cenameUpper}.html
+envsubst '${extname} ${cename} ${cenameUpper} ${cetitle} ${cedescription}' < $bindir/lib/irre/BE_ctype.html > $extensiondir/Resources/Private/ContentElements/BackendPreviews/BE_${cenameUpper}.html
 printf "Backend Template Ready" && sleep 0.1 && printf "." && sleep 0.1 && printf "." && sleep 0.1 && printf ".\n"
 
 ## Frontend Template
-envsubst '${extname} ${cename} ${cenameUpper} ${cetitle} ${cedescription}' < $bindir/lib/irre/FE_ctype.html > $extensiondir/Resources/Private/Templates/ContentElements/FE_${cenameUpper}.html
+envsubst '${extname} ${cename} ${cenameUpper} ${cetitle} ${cedescription}' < $bindir/lib/irre/FE_ctype.html > $extensiondir/Resources/Private/ContentElements/Templates/FE_${cenameUpper}.html
 printf "Frontent Template Ready" && sleep 0.1 && printf "." && sleep 0.1 && printf "." && sleep 0.1 && printf ".\n"
 
 ## TCA
-envsubst '${extname} ${cename} ${cenameUpper} ${cetitle} ${cedescription}' < $bindir/lib/irre/tt_content_ctype.php > $extensiondir/Configuration/TCA/tt_content_${cename}.php
+mkdir -p $extensiondir/Configuration/TCA/Overrides
+envsubst '${extname} ${cename} ${cenameUpper} ${cetitle} ${cedescription}' < $bindir/lib/irre/tt_content_ctype.php > $extensiondir/Configuration/TCA/Overrides/tt_content_${cename}.php
 printf "TCA Override Ready" && sleep 0.1 && printf "." && sleep 0.1 && printf "." && sleep 0.1 && printf ".\n"
 
 envsubst '${extname} ${cename} ${cenameUpper} ${cetitle} ${cedescription}' < $bindir/lib/irre/tx_table.php > $extensiondir/Configuration/TCA/tx_${cename}.php
 printf "TCA tx_${cename}.php Ready" && sleep 0.1 && printf "." && sleep 0.1 && printf "." && sleep 0.1 && printf ".\n"
 
 ## Page TS
-envsubst '${extname} ${cename} ${cenameUpper} ${cetitle} ${cedescription}' < $bindir/lib/irre/pagets_ce_wizard.t3s > $extensiondir/Configuration/PageTS/ContentElements/pagets_${cename}.t3s
+envsubst '${extname} ${cename} ${cenameUpper} ${cetitle} ${cedescription}' < $bindir/lib/irre/pagets_ce_wizard.t3s > $extensiondir/Configuration/TSconfig/ContentElements/${cename}.typoscript
 printf "PageTS Ready" && sleep 0.1 && printf "." && sleep 0.1 && printf "." && sleep 0.1 && printf ".\n"
 
 ## Typoscript
-envsubst '${extname} ${cename} ${cenameUpper} ${cetitle} ${cedescription}' < $bindir/lib/irre/typoscript_ce_ctype.t3s > $extensiondir/Configuration/TypoScript/ContentElements/typoscript_${cename}.t3s
+envsubst '${extname} ${cename} ${cenameUpper} ${cetitle} ${cedescription}' < $bindir/lib/irre/typoscript_ce_ctype.t3s > $extensiondir/Configuration/TypoScript/ContentElements/${cename}.typoscript
 printf "Typoscript Ready" && sleep 0.1 && printf "." && sleep 0.1 && printf "." && sleep 0.1 && printf ".\n"
 
 ##SQL
@@ -39,11 +40,11 @@ envsubst '${extname} ${cename} ${cenameUpper} ${cetitle} ${cedescription}' < $bi
 printf "ext_tables.php Ready" && sleep 0.1 && printf "." && sleep 0.1 && printf "." && sleep 0.1 && printf ".\n"
 
 ## SCSS
-mkdir -p $extensiondir/Resources/Public/Css/scss_includes/content-elements
-cp $bindir/lib/basic/_ctype.scss $extensiondir/Resources/Public/Css/scss_includes/content-elements/_${cename}.scss
-if ! [ -f "$extensiondir/Resources/Public/Css/scss_includes/_content-elements.scss" ]
+mkdir -p $extensiondir/Resources/Build/Assets/Scss/content-elements
+cp $bindir/lib/irre/_ctype.scss $extensiondir/Resources/Build/Assets/Scss/content-elements/_ce-${cename}.scss
+if ! [ -f "$extensiondir/Resources/Build/Assets/Scss/content-elements/_ce-includes.scss" ]
     then
-        touch $extensiondir/Resources/Public/Css/scss_includes/_content-elements.scss
+        touch $extensiondir/Resources/Build/Assets/Scss/content-elements/_ce-includes.scss
 fi
-echo "@import 'content-elements/_${cename}.scss';" >> $extensiondir/Resources/Public/Css/scss_includes/_content-elements.scss
+echo "@import 'ce-${cename}';" >> $extensiondir/Resources/Build/Assets/Scss/content-elements/_ce-includes.scss
 printf "SCSS Ready" && sleep 0.1 && printf "." && sleep 0.1 && printf "." && sleep 0.1 && printf ".\n"
